a9ad5bf9a33fe73f38e0db7b8a9e42bb19d8a8fa
akshanshck <123233309+akshanshck@users.noreply.github.com>
2023-02-04 22:46:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 #
+echo "[!] Deploying..."
 rm -rf public/
 HUGO_ENV="production" hugo --gc || exit 1
 s3deploy -source=public/ -region=eu-west-1 -bucket=bep.is -distribution-id=E8OKNT7W9ZYZ2 -path temp/td
